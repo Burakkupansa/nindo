9407af1029014b7b1d354486d171dae291ebb3da
Burakkupansa <40369505+Burakkupansa@users.noreply.github.com>
2019-01-08 18:24:51 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,9 +1,10 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
+const token = process.env.token;
 
 bot.on("ready", function() {
     bot.user.setGame("Eduquer ses petits Shinobi. | nhelp");
     console.log("Le bot a bien ete connecte");
 });
 
-bot.login("process.env.TOKEN");
+bot.login("token");
